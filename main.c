c78c0b71c07f84458a160dc22796fbecd36b9a3b
ip815s10 <pukan05@yandex.ru>
2019-03-19 16:06:03 +0700

    cin>>circle(x,y,r),P,S

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 void Fillfg(char A[], char h[], int& i);
 void Fillxch(char A[], char h[], int& i);
 void Fillych(char A[], char h[], int& i);
@@ -8,24 +9,23 @@ void Fillrch(char A[], char h[], int& i);
 int main() {
 	char ch[100], xch[10], ych[10], rch[10], fg[10];
 	double s = 0, p = 0;
-printf("Vvedite 'circle(x,y,r)' \n"
-gets(h);
-memset (fg,0,sizeof(fg));
-memset (xch,0,sizeof(xch));
-memset (ych,0,sizeof(ych));
-memset (rch,0,sizeof(rch));
-Fillfg (fg,h,i);
-Fillxch (xch,h,i);
-Fillych (ych,h,i);
-Fillrch (rch,h,i);
-x=atoi(xch);
-y=atoi(ych);
-r=atoi(rch);
-puts(ch);
-printf("\n");
-s=r*r*M_PI;
-printf("Area = %.4f",s);
-p=2*M_PI*r;
+	int i = 0, r;
+	printf("Vvedite 'circle(x,y,r)'\n \n");
+	fgets(ch, 100, stdin);
+	puts(ch);
+	memset(fg, 0, sizeof(fg));
+	memset(xch, 0, sizeof(xch));
+	memset(ych, 0, sizeof(ych));
+	memset(rch, 0, sizeof(rch));
+	Fillfg(fg, ch, i);
+	Fillxch(xch, ch, i);
+	Fillych(ych, ch, i);
+	Fillrch(rch, ch, i);
+	r = atoi(rch);
+	s = r * r * M_PI;
+	printf("Area = %.4f", s);
+	p = 2 * M_PI * r;
+	printf("\nPerimetr= %.4f\n", p);
 }
 void Fillfg(char A[], char h[], int& i) {
 	int k = 0;
