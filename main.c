a9be9d0c6a2d47fa402b28b0e1a1ffc329e9285a
ip815s10 <pukan05@yandex.ru>
2019-03-19 15:17:04 +0700

    Create Ch[100],memset mass

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	char ch[100], xch[10], ych[10], rch[10], fg[10];
+printf("Vvedite 'circle(x,y,r)' \n"
+gets(h);
+memset (fg,0,sizeof(fg));
+memset (xch,0,sizeof(xch));
+memset (ych,0,sizeof(ych));
+memset (rch,0,sizeof(rch));
+}
